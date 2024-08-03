f9002db4491ffed4ca1ce6ee3d2ac2ea4d8e84dc
Ibrahem-Khaled <xx.vvv48@gmail.com>
2024-08-03 19:53:31 +0200

    get translators

--- app/Http/Controllers/api/booksController.php
+++ app/Http/Controllers/api/booksController.php
@@ -14,7 +14,7 @@ class booksController extends Controller
     public function show($id)
     {
         $user = auth()->guard('api')->user();
-        $book = Book::find($id)->with('bookInfo.author');
+        $book = Book::with('bookInfo.author')->find($id);
 
         if (!$book) {
             return response()->json(['message' => 'Book not found'], 404);
